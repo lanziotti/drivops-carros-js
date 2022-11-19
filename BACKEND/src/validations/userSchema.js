6ebf46da8c06ab7506bc96797fe35a9b77de4d74
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-11-19 14:21:49 -0300

    Correção no userSchema

--- BACKEND/src/validations/userSchema.js
+++ BACKEND/src/validations/userSchema.js
@@ -10,11 +10,10 @@ const userSchema = joi.object({
         'string.empty': 'Todos os campos são obrigatórios.',
         'string.email': 'O campo email precisa ter um formato válido.'
     }),
-    senha: joi.string().min(8).regex(/^(?=.*?[0-9]).*$/).regex(/^(?=.*?[A-Z]).*$/).required().messages({
+    senha: joi.string().min(8).regex(/^(?=.*?[A-Z]).*$/).required().messages({
         'any.required': 'Todos os campos são obrigatórios.',
         'string.empty': 'Todos os campos são obrigatórios.',
         'string.min': 'A senha precisa ter, no mínimo, 8 caracteres',
-        'string.pattern.base': 'A senha precisa ter pelo menos um NÚMERO',
         'string.pattern.base': 'A senha precisa ter pelo menos uma LETRA MAIÚSCULA'
     })
 });
